923f8dea78818a2b8581b8fc3384b7857fb26f13
Your a <a@example.com>
2024-06-27 22:42:15 +0300

    rc

--- utils/reactive.js
+++ utils/reactive.js
@@ -17,23 +17,28 @@ export function useReactive(key, items) {
 }
 export function HookRouter(params) {
     let defaultParams = JSON.parse(JSON.stringify(params));
+    let isUpdatingFromPush = false;
 
     if (defaultParams) {
         const router = useRouter();
         updateParamsFromHash(params, defaultParams);
-        router.afterEach(() => updateParamsFromHash(params, defaultParams));
+        router.afterEach((to, from) => {
+            if (!isUpdatingFromPush) {
+                updateParamsFromHash(params, defaultParams);
+            }
+            isUpdatingFromPush = false;
+        });
     }
 
     const watchers = Object.keys(params || {}).reduce((watchers, key) => {
         if (!key.startsWith('_')) return watchers;
-
         const router = useRouter();
-
-        watchers[key] = function (newValue) {
+        watchers[key] = function (newValue, oldValue) {
             const query = { ...router.currentRoute.value.query, [key]: JSON.stringify(newValue) };
             clearTimeout(globalThis.updateQueryTimeout);
             
             globalThis.updateQueryTimeout = setTimeout(() => {
+                isUpdatingFromPush = true;
                 router.push({ query });
             }, 1500);
         };
@@ -42,20 +47,26 @@ export function HookRouter(params) {
     }, {});
 
     Object.keys(watchers).forEach(key => {
-        watch(() => params[key], watchers[key]);
+        watch(() => params[key], watchers[key], { deep: true, flush: 'sync' });
     });
 }
+
 const updateParamsFromHash = (params, defaultParams) => {
     const router = useRouter();
     const hashParams = new URLSearchParams(router.currentRoute.value.query);
     for (let key in params) {
         if (hashParams.has(key)) {
             try {
-                params[key] = JSON.parse(hashParams.get(key));
+                const newValue = JSON.parse(hashParams.get(key));
+               // if (JSON.stringify(params[key]) !== JSON.stringify(newValue)) 
+                    params[key] = newValue;
+                
             } catch (e) {
-                params[key] = hashParams.get(key);
+                //if (JSON.stringify(params[key]) !== JSON.stringify(hashParams.get(key))) 
+                    params[key] = hashParams.get(key);
+                
             }
-        } else {
+        } else if (params[key] !== defaultParams[key]) {
             params[key] = defaultParams[key];
         }
     }
