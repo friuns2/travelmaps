f3b8ea0a080e485b287ed320518682c40ba3d14d
moksha-hub <mokshagnak004@gmail.com>
2024-06-28 10:55:44 +0530

    Removed embedded repository 'maps_wea'

--- app.vue
+++ app.vue
@@ -1,8 +1,12 @@
 <template>
-    <head>
+    <div>
+      <head>
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        
-    </head>
-    <NuxtPage />
-
-</template>
+      </head>
+      <NuxtPage />
+    </div>
+  </template>
+  
+  <script setup>
+  import EmergencyContacts from '~/components/EmergencyContacts.vue'
+  </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,30 +1,42 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-
+  devtools: { enabled: false },
+  
   nitro: {
     preset: "cloudflare-pages"
   },
-
+  
   modules: ['@nuxtjs/tailwindcss', "@nuxt/scripts"],
+  
   tailwindcss: {
     config: {
       plugins: [require('daisyui')]
     }
   },
-
+  
   ssr: false,
-
+  
+  runtimeConfig: {
+    public: {
+      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY
+    }
+  },
+  
   app: {
     head: {
       script: [
         {
-          src: `https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_API_KEY}&libraries=places`,
+          src: 'https://maps.googleapis.com/maps/api/js?key=',
           async: true,
           defer: true
-        },        
-        //{  src: 'maps.js',defer: false}
+        },
+        // { src: 'maps.js', defer: false }
       ]
-    } 
+    }
+  },
+
+  // Add this to ensure environment variables are loaded
+  build: {
+    transpile: ['@googlemaps/js-api-loader']
   }
 })
